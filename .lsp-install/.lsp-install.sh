24f4728feb0685846287d4cb3d4121cc5eca0782
yeyee2901 <yhxzen2901@gmail.com>
2021-09-02 15:15:00 +0700

    ADD: install steps for the rest of Lang-servers (from apt, pip, cargo)

--- .lsp-install/.lsp-install.sh
+++ .lsp-install/.lsp-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Language Servers from NPM registry
 npm i -g install \
     bash-language-server \
     diagnostic-languageserver \
@@ -12,3 +13,13 @@ npm i -g install \
     typescript-language-server \
     vim-language-server \
     vscode-langservers-extracted
+
+# Language Servers from APT repository
+sudo apt install ccls
+
+# Language Servers from PIP registry
+pip install cmake-language-server
+
+# Language Servers from Rust Crates
+cargo install tectonic rust_analyzer
+cargo install --git https://github.com/latex-lsp/texlab.git --locked
