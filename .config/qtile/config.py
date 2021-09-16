9c06cf9f2169541217b9b80d4c05bc5b4dbf4d70
yeyee2901 <yhxzen2901@gmail.com>
2021-09-16 09:06:01 +0700

    ADD: Alacritty

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -14,10 +14,10 @@ from typing import List  # noqa: F401
 from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
-from libqtile.utils import guess_terminal
+# from libqtile.utils import guess_terminal
 
 mod = "mod4"
-terminal = guess_terminal()
+terminal = "alacritty"
 
 WALLPAPER = "~/Pictures/pop/nord_circuit.png"
 WALLPAPER_MODE = "fill"
