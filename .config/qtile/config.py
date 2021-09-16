5ff57674a45de9abb50f34ac15e26b8a38e2e93c
yeyee2901 <yhxzen2901@gmail.com>
2021-09-16 11:04:16 +0700

    ADD: compton & cava

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -10,8 +10,10 @@
 # yeyee2901 - 14 Sept 2021
 
 from typing import List  # noqa: F401
+import subprocess
+import os
 
-from libqtile import bar, layout, widget
+from libqtile import bar, layout, widget, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 # from libqtile.utils import guess_terminal
@@ -36,6 +38,9 @@ nord = {
     ]
 
 }
+
+
+
 
 #### MAPPINGS -----------------------------------------------------
 keys = [
@@ -470,16 +475,14 @@ auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
 
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
 auto_minimize = True
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
+
+#### AUTOSTART ---------------------------------------------------
+@hook.subscribe.startup_once
+def start_once():
+    home = os.path.expanduser('~')
+    subprocess.call([home + '/.config/qtile/autostart.sh'])
+
+
 wmname = "LG3D"
